036c3a4305927fb2bc049855e4ac749c9316a8ba
Tomas Kudlicka <tomas.kudlicka@avito.cz>
2020-12-08 15:41:05 +0100

    first release

---
+++ formula/quasar.rb
@@ -0,0 +1,18 @@
+require "formula"
+
+class Quasar < Formula
+  desc "CLI pro komunikaci s můstkem"
+  homepage "https://github.com/tkudlickaavitocz/quasar-cli"
+  url "../quasar-darwin-amd64-0.1.4.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "c4bb34abd18ef743572c085fcbaf0a4d5c0770a7c889ae5ce422213bb35e80d4"
+  head "https://github.com/tkudlickaavitocz/quasar-cli"
+
+  def install
+    bin.install "quasar"
+  end
+
+  # Homebrew requires tests.
+  test do
+    assert_match "quasar version 0.0.1", shell_output("#{bin}/quasar -v", 2)
+  end
+end
