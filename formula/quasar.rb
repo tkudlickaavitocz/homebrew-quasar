cccbb6494c3b43f6c421da25f19a08fbc04f08dd
Tomas Kudlicka <tomas.kudlicka@avito.cz>
2020-12-08 16:21:41 +0100

    INIT

--- formula/quasar.rb
+++ formula/quasar.rb
@@ -3,7 +3,7 @@ require "formula"
 class Quasar < Formula
   desc "CLI pro komunikaci s můstkem"
   homepage "https://github.com/tkudlickaavitocz/homebrew-quasar-cli"
-  url "../quasar-darwin-amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  url "../quasar-darwin-amd64.tar.gz", :using => :git, :tag => "0.0.1"
   sha256 "c4bb34abd18ef743572c085fcbaf0a4d5c0770a7c889ae5ce422213bb35e80d4"
   head "https://github.com/tkudlickaavitocz/homebrew-quasar-cli"
 
