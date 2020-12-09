a2b5a2a834d6766581903c8fc2b67b7826c1b774
Tomas Kudlicka <tomas.kudlicka@avito.cz>
2020-12-09 09:03:11 +0100

    WIP

--- formula/quasar.rb
+++ formula/quasar.rb
@@ -5,7 +5,6 @@ class Quasar < Formula
   homepage "https://github.com/tkudlickaavitocz/homebrew-quasar-cli"
   url "https://github.com/tkudlickaavitocz/homebrew-quasar/archive/v0.0.1.tar.gz"
   sha256 "c4bb34abd18ef743572c085fcbaf0a4d5c0770a7c889ae5ce422213bb35e80d4"
-  head "https://github.com/tkudlickaavitocz/homebrew-quasar-cli"
 
   def install
     bin.install "quasar"
